1eb1d74dd50f9b4f5f9dfa94a67abb251f1739cf
Felix (xq) Queißner <git@mq32.de>
2020-12-25 18:11:29 +0100

    Adds build support for windows.

---
+++ README.md
@@ -0,0 +1,47 @@
+# SDL2 with Zig
+
+This is a simple example project that renders the classic *bouncing DVD logo* with SDL2. This example is a minimal example on how to set up a fully deployable project with Zig.
+
+![Screenshot](https://mq32.de/public/9223d8240a1d75cf9387243a0696efd882aab3b0.png)
+
+## Building
+
+This example requires **Zig 0.7.1**, you can obtain a copy for your system [here](https://ziglang.org/download/).
+
+**Do not download a `master` build! There is currently something broken in `translate-c` that makes it impossible to import SDL2 headers!**
+
+### Linux
+Install the development packages listed below and invoke `zig build`.
+
+#### Packages (Debian)
+- `libsdl2-dev`
+- `libsdl2-image-dev`
+
+#### Packages (Arch Linux / Manjaro)
+- `sdl2`
+- `sdl2_image`
+
+### Windows
+Windows has no pre-built SDL2 neither a package manager. This means you have to download the development libraries from [libsdl.org](https://www.libsdl.org/download-2.0.php) for either *Visual C++ 32/64-bit* or *MinGW 32/64-bit* and extract it into a folder.
+
+The same must be done for the [`SDL_image` development libraries](https://www.libsdl.org/projects/SDL_image/). Chose the same abi/compiler as your SDL2 download above.
+
+Then, use
+```
+zig build -Dsdl-sdk=C:\Users\MYUSERNAME\...\SDL2-2.X.Y -Dsdl-image-sdk=C:\Users\MYUSERNAME\...\SDL2_image-2.Z.W
+```
+to build your project. The build script will auto-detect depending on your target if the *Visual C++ 32/64-bit* or *MinGW 32/64-bit* folder layout should be used.
+
+### Cross-Build (Host: Linux, Target: Windows)
+Just follow the instructions for building for *Windows*, but use *MinGW 32/64-bit*. Using *Visual C++ 32/64-bit* will **not** work!
+
+### Cross-Build (Host: Windows, Target: Linux)
+Not supported atm. Use WSL for this.
+
+## Known Problems
+
+### `lld-link: error: undefined symbol: ___chkstk_ms`
+For the target `target=i386-windows-gnu` this error is emitted. This is probably missing in `compiler_rt`
+
+### `***-windows-msvc`
+This target is not tested yet
--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ const pkgs = struct {
     };
 };
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
@@ -23,11 +23,152 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkLibC();
 
     if (target.isWindows()) {
-        // link windows
+        // linking on windows is sadly not as trivial as on linux:
+        // we have to respect 4 different configurations {x86,x64}*{msvc,mingw}
+        const sdk_path = b.option([]const u8, "sdl-sdk", "The path to the SDL2 sdk") orelse @panic("sdl-sdk must be given for windows builds!");
+        const sdk_image_path = b.option([]const u8, "sdl-image-sdk", "The path to the SDL2_image sdk") orelse @panic("sdl-image-sdk must be given for windows builds!");
+        const sdl_static = if (target.getAbi() == .msvc)
+            false
+        else
+            b.option(bool, "sdl-static", "Choses whether to link SDL statically or dynamically. Default is dynamic") orelse false;
+
+        // compute and add the include paths for both SDL2 and SDL_image
+        const include_path = if (target.getAbi() == .msvc)
+            "include/SDL2"
+        else if (target.getCpuArch() == .x86_64)
+            "x86_64-w64-mingw32/include/SDL2"
+        else
+            "i686-w64-mingw32/include/SDL2";
+
+        exe.addIncludeDir(try std.fs.path.join(b.allocator, &[_][]const u8{
+            sdk_path,
+            include_path,
+        }));
+        exe.addIncludeDir(try std.fs.path.join(b.allocator, &[_][]const u8{
+            sdk_image_path,
+            include_path,
+        }));
+
+        // link the right libraries
+
+        if (target.getAbi() == .msvc) {
+            // MSVC uses lib/$ARCH as paths:
+            const library_path = if (target.getCpuArch() == .x86_64)
+                "lib/x64/"
+            else
+                "lib/x86/";
+
+            // and links those as normal libraries
+            exe.addLibPath(try std.fs.path.join(b.allocator, &[_][]const u8{
+                sdk_path,
+                library_path,
+            }));
+            exe.addLibPath(try std.fs.path.join(b.allocator, &[_][]const u8{
+                sdk_image_path,
+                library_path,
+            }));
+
+            exe.linkSystemLibrary("SDL2");
+            exe.linkSystemLibrary("SDL2_image");
+        } else {
+            // mingw uses either static or dynamically built object files
+            const library_path = if (target.getCpuArch() == .x86_64)
+                "x86_64-w64-mingw32/lib"
+            else
+                "i686-w64-mingw32/lib";
+
+            exe.addObjectFile(try std.fs.path.join(b.allocator, &[_][]const u8{
+                sdk_path,
+                library_path,
+                if (sdl_static)
+                    "libSDL2.a"
+                else
+                    "libSDL2.dll.a",
+            }));
+            exe.addObjectFile(try std.fs.path.join(b.allocator, &[_][]const u8{
+                sdk_image_path,
+                library_path,
+                if (sdl_static)
+                    "libSDL2_image.a"
+                else
+                    "libSDL2_image.dll.a",
+            }));
+
+            if (sdl_static) {
+                // link all system libraries for SDL2
+                const static_libs = [_][]const u8{
+                    "setupapi",
+                    "user32",
+                    "gdi32",
+                    "winmm",
+                    "imm32",
+                    "ole32",
+                    "oleaut32",
+                    "shell32",
+                    "version",
+                    "uuid",
+                };
+                for (static_libs) |lib|
+                    exe.linkSystemLibrary(lib);
+            }
+        }
+
+        // copy the right files into the output directory:
+
+        const binary_path = if (target.getAbi() == .msvc)
+            if (target.getCpuArch() == .x86_64)
+                "lib/x64"
+            else
+                "lib/x86"
+        else if (target.getCpuArch() == .x86_64)
+            "x86_64-w64-mingw32/bin"
+        else
+            "i686-w64-mingw32/bin";
+
+        if (!sdl_static) {
+            b.installBinFile(
+                try std.fs.path.join(b.allocator, &[_][]const u8{
+                    sdk_path,
+                    binary_path,
+                    "SDL2.dll",
+                }),
+                "SDL2.dll",
+            );
+            b.installBinFile(
+                try std.fs.path.join(b.allocator, &[_][]const u8{
+                    sdk_image_path,
+                    binary_path,
+                    "SDL2_image.dll",
+                }),
+                "SDL2_image.dll",
+            );
+        }
+
+        // required DLLs for sdl_image
+        const image_dlls = [_][]const u8{
+            "libjpeg-9.dll",
+            "libpng16-16.dll",
+            "libtiff-5.dll",
+            "libwebp-7.dll",
+            "zlib1.dll",
+        };
+
+        for (image_dlls) |dll| {
+            b.installBinFile(
+                try std.fs.path.join(b.allocator, &[_][]const u8{
+                    sdk_image_path,
+                    binary_path,
+                    dll,
+                }),
+                std.fs.path.basename(dll),
+            );
+        }
     } else if (target.isLinux()) {
-        // on linux, we should rely on the system libraries
+        // on linux, we should rely on the system libraries to "just work"
         exe.linkSystemLibrary("sdl2");
         exe.linkSystemLibrary("SDL2_image");
+    } else {
+        @panic("Chosen OS is not supported yet!");
     }
 
     exe.install();
